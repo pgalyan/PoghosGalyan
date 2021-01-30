731a53f12ffe2b5ba7ca23fb5fec4ad839dc4495
pgalyan <poghosgalyan@gmail.com>
2021-01-30 16:14:42 +0400

    commit lesson 2

---
+++ src/Components/component1.js
@@ -0,0 +1,11 @@
+function Comp1() {
+    return(
+        <div>
+            <div className='component1'>
+               <p>Component 1</p>
+            </div>
+        </div>
+    );
+}
+
+export default Comp1;
---
+++ src/Components/component2.js
@@ -0,0 +1,11 @@
+function Comp2() {
+    return(
+        <div>
+            <div className='component2'>
+               <p>Component 2</p>
+            </div>
+        </div>
+    );
+}
+
+export default Comp2;
